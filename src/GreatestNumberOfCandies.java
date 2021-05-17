022a72b6d8b67e1ae35de78762d393bedbbbe6f7
Kalle <domare2@hotmail.com>
2021-05-17 22:16:00 +0200

    greatest number of candies

---
+++ src/GreatestNumberOfCandies.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kalle
+ */
+public class GreatestNumberOfCandies {
+    int extraCandies = 0;
+    public GreatestNumberOfCandies(int extraCandies){
+        this.extraCandies = extraCandies;
+    }
+    
+    public boolean KidsCandies(int[] candies){
+        
+        for (int i = 0; i< candies.length; i++){
+            
+        
+        
+    }
+        return true;
+   }
+    
+    
+    
+}
