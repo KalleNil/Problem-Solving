a512982d1d90dd69b6d11af48d6d3622ec881306
Kalle <domare2@hotmail.com>
2021-05-23 16:17:28 +0200

    the amazing human cannonball

--- src/TheAmazingHumanCannonball.java
+++ src/TheAmazingHumanCannonball.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,47 @@
  * @author Kalle
  */
 public class TheAmazingHumanCannonball {
+    
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int testCases = scan.nextInt();
+        
+        for (int i= 0; i<testCases; i++){
+            Scanner scanloop = new Scanner(System.in);
+            
+            
+            
+            String[] currentTest = scanloop.nextLine().split(" ");
+            
+            try {
+                
+            } catch (Exception e) {
+            }
+            
+            float initialVelocity =(float) Integer.parseInt(currentTest[0]);
+            float angle = (float)Integer.parseInt(currentTest[1]);
+            float distanceFromWall = (float)Integer.parseInt(currentTest[2]);
+            float lowerEdge = (float)Integer.parseInt(currentTest[3]);
+            float upperEdge =(float)Integer.parseInt(currentTest[4]);
+            
+            double radien = Math.toRadians(angle);
+            double x = distanceFromWall/ (initialVelocity*Math.cos(radien));
+            double y = (initialVelocity * x * Math.sin(radien)) - (4.905*x*x);
+            
+            if (lowerEdge +1 <= y && y <= upperEdge -1){
+                System.out.println("safe");
+            }else{
+                System.out.println("not safe");
+            }
+            
+            
+                
+        }
+        
+        
+    }
+  
     
 }
